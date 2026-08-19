78e9940482f5a74e2c0bf465c2184bb692b7a665
76ht22357-108-phamdinhthanh <thanhpham20072311@gmail.com>
2026-08-19 18:16:55 +0700

    BT01-CP1: Tao cau truc

---
+++ bai1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// 1. Khai báo kiểu dữ liệu cấu trúc Nhân viên
+struct NhanVien {
+    string maNV;
+    string tenNV;
+    double luongCoBan;
+    double heSo;
+};
+
+int main() {
+    int n;
+    // Khai báo mảng chứa danh sách nhân viên
+    NhanVien ds[100];
+
+    return 0;
+}
